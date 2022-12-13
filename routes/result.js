d3c6d45b09374278f979927ac57073287666e234
oaayoub <oaayoub0101@GMAIL.COM>
2022-12-13 19:45:20 +0200

    almost db

--- routes/result.js
+++ routes/result.js
@@ -5,6 +5,19 @@ router.use(express.json());
 router.get('/' , async (req, res) => {
         console.log("result  page requested");
         console.log("req.query : ", req.query);
+
+        //you will know the query type from req.query[type]
+
+
+        //incase if it's query 
+        //get the query table 
+        //from the db and render the answer
+
+
+         //incase if it's post 
+        //post to the db .. check if there violation 
+        // return post status
+
         res.render('table');
    
     
